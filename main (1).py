05dc7f9971ac9ebaef72e9c19367a783eb5f01a2
Sudhasiva10062004 <145090524+Sudhasiva10062004@users.noreply.github.com>
2023-09-15 16:41:38 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,6 +1,5 @@
-def factorial(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * factorial(n - 1)
-print(factorial(5))
+year=int(input("enter year to be checked:"))
+if(year%4==0 and year%100!=0 or year%400==0):
+  print ("the year is a leap year!")
+else:
+  print("the year isn't a leap")
