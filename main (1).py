eba133d3ca0bfa59af3decdda5c5f6053829a1ac
Sudhasiva10062004 <145090524+Sudhasiva10062004@users.noreply.github.com>
2023-10-16 07:26:07 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,5 +1,24 @@
-year=int(input("enter year to be checked:"))
-if(year%4==0 and year%100!=0 or year%400==0):
-  print ("the year is a leap year!")
-else:
-  print("the year isn't a leap")
+class Bank_Account:
+    def __init__(self, account_number, account_name, account_balance):  
+      self.__account_number = account_number
+      self.__account_name = account_name
+      self.__account_balance = account_balance
+    def deposit (self,amount):
+      if  amount > 0 :
+        self.__account_balance += amount
+        print(f"deposited:₹{amount}. current balance:₹{self.__account_balance}")
+      else:
+        print("Invaial deposit amount")
+    def withdraw (self,amount):
+      if amount > 0 and amount<=self.__account_balance :
+        self.__account_balance -= amount
+        print(f"withdraw:₹{amount}. current balance:₹{self.__account_balance}")
+      else:
+        print("Invalid withdraw amount")
+        
+    def display (self):
+     print( f'account_balance of { self.__account_name }Rs.{ self.__account_balance}')
+account=Bank_Account(123456,"Ramya",500)
+account.deposit(1000)
+account.withdraw(300)
+account.display()
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def factorial(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * factorial(n - 1)
-print(factorial(5))
+def linear_search_product(product_list, target_product):
+    indices = []
+    for i, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(i)
+    return indices
+
+# Example usage:
+products = ["apple", "banana", "orange", "apple", "grape", "apple"]
+target = "apple"
+result = linear_search_product(products, target)
+print(result)
