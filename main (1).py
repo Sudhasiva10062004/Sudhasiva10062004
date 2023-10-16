d1b023ed0edd21429b908294ac22150a754abbdd
Sudhasiva10062004 <145090524+Sudhasiva10062004@users.noreply.github.com>
2023-10-16 07:37:55 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,24 +1,6 @@
-class Bank_Account:
-    def __init__(self, account_number, account_name, account_balance):  
-      self.__account_number = account_number
-      self.__account_name = account_name
-      self.__account_balance = account_balance
-    def deposit (self,amount):
-      if  amount > 0 :
-        self.__account_balance += amount
-        print(f"deposited:₹{amount}. current balance:₹{self.__account_balance}")
-      else:
-        print("Invaial deposit amount")
-    def withdraw (self,amount):
-      if amount > 0 and amount<=self.__account_balance :
-        self.__account_balance -= amount
-        print(f"withdraw:₹{amount}. current balance:₹{self.__account_balance}")
-      else:
-        print("Invalid withdraw amount")
-        
-    def display (self):
-     print( f'account_balance of { self.__account_name }Rs.{ self.__account_balance}')
-account=Bank_Account(123456,"Ramya",500)
-account.deposit(1000)
-account.withdraw(300)
-account.display()
+def factorial(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * factorial(n - 1)
+print(factorial(5))
